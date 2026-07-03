6eeb2bd9ba70b328a9c3ec31d15de1a2186afae7
Victoria <victoriabertero001@gmail.com>
2026-07-02 23:11:51 -0300

    Cambios en la UI

--- src/main/java/com/victoria/Interfaces/MenuPrincipal.java
+++ src/main/java/com/victoria/Interfaces/MenuPrincipal.java
@@ -1,79 +1,97 @@
 package com.victoria.Interfaces;
 
-import java.io.IOException;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.geometry.Side;
 
 import com.victoria.navegation.Navegador;
 
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.stage.Stage;
 
 public class MenuPrincipal {
 
     @FXML private Button btnAltaProductos;
     @FXML private Button btnEmpleados;
-    @FXML private MenuItem menuItemGestionRopa;
-    @FXML private MenuItem menuItemGestionAccs;
-    @FXML private MenuItem menuItemVisualizacionRopa;
-    @FXML private MenuItem menuItemVisualizacionAccs;
-    @FXML private MenuBar menuBarGestion;
+    @FXML private Button btnGestionStock;
+    @FXML private Button btnVisualizacionStock;
+    @FXML private Button btnVentas;
 
     @FXML
     public void initialize() {
-        
-        btnAltaProductos.setOnAction(e -> abrirAltaProducto());
-        btnEmpleados.setOnAction(e -> abrirGestionEmpleado());
-
-        menuItemGestionRopa.setOnAction(this::abrirGestionRopa);
-        menuItemGestionAccs.setOnAction(this::abrirGestionAccs);
-        
-        menuItemVisualizacionRopa.setOnAction(this::abrirVisualizacionRopa);
-        menuItemVisualizacionAccs.setOnAction(this::abrirVisualizacionAccs);
-       
-        
+        // ya no hace falta setOnAction manual: el FXML llama directo a los métodos vía onAction
     }
 
     @FXML
-    private void abrirGestionEmpleado(){
-      Navegador.cambiarVista("/com/victoria/Interfaces/SceneGestionEmpleados.fxml");
+    private void abrirGestionEmpleado(ActionEvent event){
+        Navegador.cambiarVista("/com/victoria/Interfaces/SceneGestionEmpleados.fxml");
     }
 
     @FXML
-    private void abrirAltaProducto() {
+    private void abrirAltaProducto(ActionEvent event) {
         Navegador.cambiarVista("/com/victoria/Interfaces/SceneAltaProducto.fxml");
     }
-  
+
     @FXML
     private void abrirGestionRopa(ActionEvent event) {
-    Navegador.cambiarVista("/com/victoria/Interfaces/SceneGestionStockRopa.fxml");
+        Navegador.cambiarVista("/com/victoria/Interfaces/SceneGestionStockRopa.fxml");
     }
+
     @FXML
     private void abrirGestionAccs(ActionEvent event) {
-    Navegador.cambiarVista("/com/victoria/Interfaces/SceneGestionStockAccs.fxml");
+        Navegador.cambiarVista("/com/victoria/Interfaces/SceneGestionStockAccs.fxml");
     }
+
     @FXML
     private void abrirVisualizacionRopa(ActionEvent event) {
-    Navegador.cambiarVista("/com/victoria/Interfaces/SceneVisualizacionStockRopa.fxml");
+        Navegador.cambiarVista("/com/victoria/Interfaces/SceneVisualizacionStockRopa.fxml");
     }
+
     @FXML
     private void abrirVisualizacionAccs(ActionEvent event) {
-    Navegador.cambiarVista("/com/victoria/Interfaces/SceneVisualizacionStockAccs.fxml");
+        Navegador.cambiarVista("/com/victoria/Interfaces/SceneVisualizacionStockAccs.fxml");
     }
 
-    private void mostrarMensaje(String mensaje) {
-        System.out.println(mensaje);
-        // Si quieres mostrar alerta visual:
-        // Alert alert = new Alert(Alert.AlertType.INFORMATION, mensaje);
-        // alert.showAndWait();
+    @FXML
+    private void mostrarMenuGestionStock(ActionEvent event) {
+        ContextMenu menu = new ContextMenu();
+
+        MenuItem itemRopa = new MenuItem("Gestión de Ropa");
+        itemRopa.setOnAction(this::abrirGestionRopa);
+
+        MenuItem itemAccs = new MenuItem("Gestión de Accesorios");
+        itemAccs.setOnAction(this::abrirGestionAccs);
+
+        menu.getItems().addAll(itemRopa, itemAccs);
+        menu.show(btnGestionStock, Side.BOTTOM, 0, 5);
     }
 
+    @FXML
+    private void mostrarMenuVisualizacion(ActionEvent event) {
+        ContextMenu menu = new ContextMenu();
+
+        MenuItem itemRopa = new MenuItem("Visualización de Ropa");
+        itemRopa.setOnAction(this::abrirVisualizacionRopa);
+
+        MenuItem itemAccs = new MenuItem("Visualización de Accesorios");
+        itemAccs.setOnAction(this::abrirVisualizacionAccs);
+
+        menu.getItems().addAll(itemRopa, itemAccs);
+        menu.show(btnVisualizacionStock, Side.BOTTOM, 0, 5);
+    }
+
+    @FXML
+    private void mostrarMenuVentas(ActionEvent event) {
+        ContextMenu menu = new ContextMenu();
+
+        MenuItem itemRegistrar = new MenuItem("Registrar Venta");
+        itemRegistrar.setOnAction(e -> Navegador.cambiarVista("/com/victoria/Interfaces/SceneRegistrarVenta.fxml"));
+
+        MenuItem itemHistorial = new MenuItem("Historial de Ventas");
+        itemHistorial.setOnAction(e -> Navegador.cambiarVista("/com/victoria/Interfaces/SceneHistorialVentas.fxml"));
+
+        menu.getItems().addAll(itemRegistrar, itemHistorial);
+        menu.show(btnVentas, Side.BOTTOM, 0, 5);
+    }
 }
